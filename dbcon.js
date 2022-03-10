ffd82c24ed2a3e3feb2273b251a2afc3580465be
jonargee19 <jrgbone@gmail.com>
2022-03-09 18:15:54 -0700

    add local mysql server connection

---
+++ dbcon.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const mysql = require('mysql')
+
+const db = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
+})
+
+module.exports = db
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
+const db = require('./dbcon')
 
 const PORT = process.env.PORT || 4000;
 
 const express = require('express');
 const app = express();
 const path = require('path');
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL server!')
+  })
  
 app.use(express.urlencoded({
     extended: true
