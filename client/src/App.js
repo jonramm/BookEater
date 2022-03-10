4891915b1c349e3f406bc83c7b33f21e8f054725
jonargee19 <jrgbone@gmail.com>
2022-03-09 18:40:48 -0700

    add basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <h1>BOOKEATER</h1>
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path='/' element={ <HomePage /> } />
+        </Routes>
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+
+    return (
+        <>
+            <h1>BookEater</h1>
+        </>
+    )
+}
+
+export default HomePage;
