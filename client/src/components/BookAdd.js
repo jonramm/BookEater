5a2d092c82060647b4afa1a4d58060a9677ac1ab
jonargee19 <jrgbone@gmail.com>
2022-06-15 14:18:05 -0600

    clickable emojis

--- client/src/components/BookAdd.js
+++ client/src/components/BookAdd.js
@@ -4,11 +4,20 @@ import { useNavigate } from "react-router-dom";
 import axios from "../api/axios";
 import AuthContext from '../context/AuthProvider'
 import Popup from 'reactjs-popup';
+import strawberry from '../assets/strawberry.PNG'
+import burger from '../assets/burger.PNG'
 
 const BookAdd = () => {
 
     const [title, setTitle] = useState('')
     const [author, setAuthor] = useState('')
+
+    const [strawberryIsChecked, setStrawberryIsChecked] = useState(false)
+    const [burgerIsChecked, setBurgerIsChecked] = useState(false)
+    const [champagneIsChecked, setChampagneIsChecked] = useState(false)
+    const [cocktailIsChecked, setCocktailIsChecked] = useState(false)
+    const [herringIsChecked, setHerringIsChecked] = useState(false)
+    const [moonshineIsChecked, setMoonshineIsChecked] = useState(false)
 
     const { auth, setAuth } = useContext(AuthContext)
 
@@ -64,8 +73,21 @@ const BookAdd = () => {
                         <form className="form-signin">
                             <p>How did {title} make you feel?</p>
                             <input
-                                type='checkbox'>
+                                className="emoji-checkbox"
+                                type='checkbox'
+                                id='cb1'
+                                value={strawberryIsChecked}
+                                onChange={(e) => { setStrawberryIsChecked(!strawberryIsChecked) }}>
                             </input>
+                            <label for="cb1" className="checkbox-label"><img className="emoji-img" src={strawberry} /></label>
+                            <input
+                                className="emoji-checkbox"
+                                type='checkbox'
+                                id='cb2'
+                                value={burgerIsChecked}
+                                onChange={(e) => { setBurgerIsChecked(!burgerIsChecked) }}>
+                            </input>
+                            <label for="cb2" className="checkbox-label"><img className="emoji-img" src={burger} /></label>
                         </form>
                         <button class="btn btn-lg btn-light home-btn btn-block">Add book and review</button>
                         <button onClick={(e) => addBook(e)} class="btn btn-lg btn-light home-btn btn-block">Skip and add to library</button>
