4538a358691315e771e2dfb884f5b777036879bd
jonargee19 <jrgbone@gmail.com>
2022-06-16 14:09:56 -0600

    book add nourishment works

--- client/src/components/BookAdd.js
+++ client/src/components/BookAdd.js
@@ -4,12 +4,20 @@ import { useNavigate } from "react-router-dom";
 import axios from "../api/axios";
 import AuthContext from '../context/AuthProvider'
 import Popup from 'reactjs-popup';
+import Carousel from 'react-bootstrap/Carousel';
+
 import strawberry from '../assets/strawberry.png'
 import burger from '../assets/burger.png'
 import herring from '../assets/herring.png'
 import moonshine from '../assets/moonshine.png'
 import cocktail from '../assets/cocktail.png'
 import champagne from '../assets/champagne.png'
+import anchovies from '../assets/anchovies.png'
+import moldyBread from '../assets/moldyBread.png'
+import poop from '../assets/poop.png'
+import redWine from '../assets/redWine.png'
+import sandwich from '../assets/sandwich.png'
+import whiskey from '../assets/whiskey.png'
 
 const BookAdd = () => {
 
@@ -22,6 +30,12 @@ const BookAdd = () => {
     const [cocktailIsChecked, setCocktailIsChecked] = useState(false)
     const [herringIsChecked, setHerringIsChecked] = useState(false)
     const [moonshineIsChecked, setMoonshineIsChecked] = useState(false)
+    const [anchoviesIsChecked, setAnchoviesIsChecked] = useState(false)
+    const [moldyBreadIsChecked, setMoldyBreadIsChecked] = useState(false)
+    const [poopIsChecked, setPoopIsChecked] = useState(false)
+    const [redWineIsChecked, setRedWineIsChecked] = useState(false)
+    const [sandwichIsChecked, setSandwichIsChecked] = useState(false)
+    const [whiskeyIsChecked, setWhiskeyIsChecked] = useState(false)
 
     const { auth, setAuth } = useContext(AuthContext)
 
@@ -46,12 +60,19 @@ const BookAdd = () => {
         e.preventDefault()
         try {
             let array = []
-            if (strawberryIsChecked) {array.push(1)}
-            if (burgerIsChecked) {array.push(2)}
-            if (herringIsChecked) {array.push(3)}
-            if (moonshineIsChecked) {array.push(4)}
-            if (cocktailIsChecked) {array.push(5)}
-            if (champagneIsChecked) {array.push(6)}
+            if (strawberryIsChecked) { array.push(1) }
+            if (burgerIsChecked) { array.push(2) }
+            if (herringIsChecked) { array.push(3) }
+            if (moonshineIsChecked) { array.push(4) }
+            if (cocktailIsChecked) { array.push(5) }
+            if (champagneIsChecked) { array.push(6) }
+            if (anchoviesIsChecked) { array.push(7) }
+            if (moldyBreadIsChecked) { array.push(8) }
+            if (poopIsChecked) { array.push(9) }
+            if (redWineIsChecked) { array.push(10) }
+            if (sandwichIsChecked) { array.push(11) }
+            if (whiskeyIsChecked) { array.push(12) }
+
             const response = await axios.post('/add-book-and-nourishment',
                 JSON.stringify({ title, author, array }),
                 {
@@ -59,7 +80,7 @@ const BookAdd = () => {
                     withCredentials: true
                 })
             navigate('/library')
-        } catch(err) {
+        } catch (err) {
             console.log(err)
         }
     }
@@ -94,58 +115,125 @@ const BookAdd = () => {
                     trigger={<button disabled={!title || !author} class="btn btn-lg btn-light next-btn btn-block">Next</button>}
                     modal
                     nested
-                    >
+                >
                     <div className="review-container">
                         <form className="form-signin">
                             <p>This book tastes like...</p>
-                            <input
-                                className="emoji-checkbox"
-                                type='checkbox'
-                                id='cb1'
-                                value={strawberryIsChecked}
-                                onChange={(e) => { setStrawberryIsChecked(!strawberryIsChecked) }}>
-                            </input>
-                            <label for="cb1" className="checkbox-label"><img className="emoji-img" src={strawberry} /></label>
-                            <input
-                                className="emoji-checkbox"
-                                type='checkbox'
-                                id='cb2'
-                                value={burgerIsChecked}
-                                onChange={(e) => { setBurgerIsChecked(!burgerIsChecked) }}>
-                            </input>
-                            <label for="cb2" className="checkbox-label"><img className="emoji-img" src={burger} /></label>
-                            <input
-                                className="emoji-checkbox"
-                                type='checkbox'
-                                id='cb3'
-                                value={herringIsChecked}
-                                onChange={(e) => { setHerringIsChecked(!herringIsChecked) }}>
-                            </input>
-                            <label for="cb3" className="checkbox-label"><img className="emoji-img" src={herring} /></label>
-                            <input
-                                className="emoji-checkbox"
-                                type='checkbox'
-                                id='cb4'
-                                value={moonshineIsChecked}
-                                onChange={(e) => { setMoonshineIsChecked(!moonshineIsChecked) }}>
-                            </input>
-                            <label for="cb4" className="checkbox-label"><img className="emoji-img" src={moonshine} /></label>
-                            <input
-                                className="emoji-checkbox"
-                                type='checkbox'
-                                id='cb5'
-                                value={cocktailIsChecked}
-                                onChange={(e) => { setCocktailIsChecked(!cocktailIsChecked) }}>
-                            </input>
-                            <label for="cb5" className="checkbox-label"><img className="emoji-img" src={cocktail} /></label>
-                            <input
-                                className="emoji-checkbox"
-                                type='checkbox'
-                                id='cb6'
-                                value={champagneIsChecked}
-                                onChange={(e) => { setChampagneIsChecked(!champagneIsChecked) }}>
-                            </input>
-                            <label for="cb6" className="checkbox-label"><img className="emoji-img" src={champagne} /></label>
+                            <Carousel interval={null}>
+                            <Carousel.Item>
+                                <input
+                                    checked={strawberryIsChecked ? 'checked' : ''}
+                                    className="emoji-checkbox"
+                                    type='checkbox'
+                                    id='cb1'
+                                    value={strawberryIsChecked}
+                                    onChange={(e) => { setStrawberryIsChecked(!strawberryIsChecked) }}>
+                                </input>
+                                <label for="cb1" className="checkbox-label"><img className="emoji-img" src={strawberry} /></label>
+                                <input
+                                    checked={burgerIsChecked ? 'checked' : ''}
+                                    className="emoji-checkbox"
+                                    type='checkbox'
+                                    id='cb2'
+                                    value={burgerIsChecked}
+                                    onChange={(e) => { setBurgerIsChecked(!burgerIsChecked) }}>
+                                </input>
+                                <label for="cb2" className="checkbox-label"><img className="emoji-img" src={burger} /></label>
+                                <input
+                                    checked={herringIsChecked ? 'checked' : ''}
+                                    className="emoji-checkbox"
+                                    type='checkbox'
+                                    id='cb3'
+                                    value={herringIsChecked}
+                                    onChange={(e) => { setHerringIsChecked(!herringIsChecked) }}>
+                                </input>
+                                <label for="cb3" className="checkbox-label"><img className="emoji-img" src={herring} /></label>
+                                <input
+                                    checked={moonshineIsChecked ? 'checked' : ''}
+                                    className="emoji-checkbox"
+                                    type='checkbox'
+                                    id='cb4'
+                                    value={moonshineIsChecked}
+                                    onChange={(e) => { setMoonshineIsChecked(!moonshineIsChecked) }}>
+                                </input>
+                                <label for="cb4" className="checkbox-label"><img className="emoji-img" src={moonshine} /></label>
+                                <input
+                                    checked={cocktailIsChecked ? 'checked' : ''}
+                                    className="emoji-checkbox"
+                                    type='checkbox'
+                                    id='cb5'
+                                    value={cocktailIsChecked}
+                                    onChange={(e) => { setCocktailIsChecked(!cocktailIsChecked) }}>
+                                </input>
+                                <label for="cb5" className="checkbox-label"><img className="emoji-img" src={cocktail} /></label>
+                                <input
+                                    checked={champagneIsChecked ? 'checked' : ''}
+                                    className="emoji-checkbox"
+                                    type='checkbox'
+                                    id='cb6'
+                                    value={champagneIsChecked}
+                                    onChange={(e) => { setChampagneIsChecked(!champagneIsChecked) }}>
+                                </input>
+                                <label for="cb6" className="checkbox-label"><img className="emoji-img" src={champagne} /></label>
+                            </Carousel.Item>
+
+                            <Carousel.Item>
+                                <input
+                                    checked={anchoviesIsChecked ? 'checked' : ''}
+                                    className="emoji-checkbox"
+                                    type='checkbox'
+                                    id='cb7'
+                                    value={anchoviesIsChecked}
+                                    onChange={(e) => { setAnchoviesIsChecked(!anchoviesIsChecked) }}>
+                                </input>
+                                <label for="cb7" className="checkbox-label"><img className="emoji-img" src={anchovies} /></label>
+                                <input
+                                    checked={moldyBreadIsChecked ? 'checked' : ''}
+                                    className="emoji-checkbox"
+                                    type='checkbox'
+                                    id='cb8'
+                                    value={moldyBreadIsChecked}
+                                    onChange={(e) => { setMoldyBreadIsChecked(!moldyBreadIsChecked) }}>
+                                </input>
+                                <label for="cb8" className="checkbox-label"><img className="emoji-img" src={moldyBread} /></label>
+                                <input
+                                    checked={poopIsChecked ? 'checked' : ''}
+                                    className="emoji-checkbox"
+                                    type='checkbox'
+                                    id='cb9'
+                                    value={poopIsChecked}
+                                    onChange={(e) => { setPoopIsChecked(!poopIsChecked) }}>
+                                </input>
+                                <label for="cb9" className="checkbox-label"><img className="emoji-img" src={poop} /></label>
+                                <input
+                                    checked={redWineIsChecked ? 'checked' : ''}
+                                    className="emoji-checkbox"
+                                    type='checkbox'
+                                    id='cb10'
+                                    value={redWineIsChecked}
+                                    onChange={(e) => { setRedWineIsChecked(!redWineIsChecked) }}>
+                                </input>
+                                <label for="cb10" className="checkbox-label"><img className="emoji-img" src={redWine} /></label>
+                                <input
+                                    checked={sandwichIsChecked ? 'checked' : ''}
+                                    className="emoji-checkbox"
+                                    type='checkbox'
+                                    id='cb11'
+                                    value={sandwichIsChecked}
+                                    onChange={(e) => { setSandwichIsChecked(!sandwichIsChecked) }}>
+                                </input>
+                                <label for="cb11" className="checkbox-label"><img className="emoji-img" src={sandwich} /></label>
+                                <input
+                                    checked={whiskeyIsChecked ? 'checked' : ''}
+                                    className="emoji-checkbox"
+                                    type='checkbox'
+                                    id='cb12'
+                                    value={whiskeyIsChecked}
+                                    onChange={(e) => { setWhiskeyIsChecked(!whiskeyIsChecked) }}>
+                                </input>
+                                <label for="cb12" className="checkbox-label"><img className="emoji-img" src={whiskey} /></label>
+                            </Carousel.Item>
+                        </Carousel>
                         </form>
                         <button onClick={(e) => addBookAndNourishment(e)} class="btn btn-lg btn-light home-btn btn-block">Add book and review</button>
                         <button onClick={(e) => addBook(e)} class="btn btn-lg btn-light home-btn btn-block">Skip and add to library</button>
