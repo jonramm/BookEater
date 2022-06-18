2c349f30554ea84c0f434957b30ee00600d04f81
jonargee19 <jrgbone@gmail.com>
2022-06-18 13:40:10 -0600

    change connection info

--- dbcon.js
+++ dbcon.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
 const mysql = require('mysql')
 
-const db = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB,
-    multipleStatements: true
-})
+let db
+
+if (process.env.JAWS_DB) {
+    db = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    db = mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DB,
+        multipleStatements: true
+    })
+}
 
 module.exports = db
--- sequelizeDbConn.js
+++ sequelizeDbConn.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
-    dialect: 'mysql'
-})
+let sequelize
+
+if (process.env.JAWS_DB) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+    sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+        dialect: 'mysql'
+    })
+}
 
 module.exports = sequelize
