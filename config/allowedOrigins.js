bf5233f149229e0de87566f335771783cafb02d2
jonargee19 <jrgbone@gmail.com>
2022-06-18 15:08:41 -0600

    fix cors allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:4000',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://bookeater-site.herokuapp.com'
 ]
 
 module.exports = allowedOrigins
