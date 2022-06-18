58e3c23f47522ca7b63864352b65d1e31f9464a4
jonargee19 <jrgbone@gmail.com>
2022-06-18 14:59:03 -0600

    db string

--- sequelizeDbConn.js
+++ sequelizeDbConn.js
@@ -6,9 +6,11 @@ let sequelize
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL)
 } else {
-    sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
-        dialect: 'mysql'
-    })
+    // sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+    //     dialect: 'mysql'
+    // })
+    sequelize = new Sequelize(process.env.MYSQL_CONNECTION_STRING)
+    
 }
 
 module.exports = sequelize
