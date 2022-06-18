8c79f4e3208ee13e1eded202a23ab098dc14bab8
jonargee19 <jrgbone@gmail.com>
2022-06-18 15:35:28 -0600

    update allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,9 @@ const allowedOrigins = [
     'http://localhost:4000',
     'http://localhost:3000',
     'https://bookeater-site.herokuapp.com',
-    'https://www.bookeater.org'
+    'https://www.bookeater.org',
+    'bookeater.org',
+    'www.bookeater.org'
 ]
 
 module.exports = allowedOrigins
