d5e9acdc13437d90861616539ee78b05784d5bd5
jonargee19 <jrgbone@gmail.com>
2022-06-19 09:31:17 -0600

    vhange axios base url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 const BASE_URL = 'http://localhost:4000'
+const PROD_URL = 'https://bookeater-site.herokuapp.com'
 
 export default axios.create({
-    baseURL: BASE_URL,
+    // baseURL: BASE_URL,
+    baseURL: PROD_URL
 })
 
 export const axiosPrivate = axios.create({
-    baseURL: BASE_URL
+    // baseURL: BASE_URL
+    baseURL: PROD_URL
 })
