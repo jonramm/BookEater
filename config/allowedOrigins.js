6e391d06723158128a215a4a6821607cc1889352
jonargee19 <jrgbone@gmail.com>
2022-06-19 09:42:02 -0600

    add more allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,9 @@
 const allowedOrigins = [
     'http://localhost:4000',
     'http://localhost:3000',
+    'https://localhost:4000',
+    'https://localhost:3000',
+    'localhost:4000',
     'http://bookeater-site.herokuapp.com',
     'http://bookeater-site.herokuapp.com',
     'http://www.bookeater.org',
